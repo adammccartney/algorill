0f9d14331cb3d85e108d9e09f3b3564744c2381c
Adam McCartney <adam@mur.at>
2023-08-27 17:00:55 +0200

    Refactor karatsuba to handle big integers
    This version uses golang's math/big library to handle big integers. The
    tests are refactored to use a multiplcation of two 64 digit strings.

--- pkg/karatsuba_mult/karatsuba_mult.go
+++ pkg/karatsuba_mult/karatsuba_mult.go
@@ -2,20 +2,17 @@ package karatsuba_mult
 
 import (
 	"math"
-	"strconv"
+	"math/big"
 )
 
 // split the integer x into two parts
-func getParts(x, n int) (int, int) {
-	divisor := int(math.Pow10(n))
+func getParts(x *big.Int, n int) (*big.Int, *big.Int) {
+	divisor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(n)), nil)
 
-	if x >= divisor {
-		pone := x / divisor
-		ptwo := x % divisor
-		return pone, ptwo
-	} else {
-		return 0, x
-	}
+	pone := new(big.Int).Div(x, divisor)
+	ptwo := new(big.Int).Mod(x, divisor)
+
+	return pone, ptwo
 }
 
 // Karatsuba mult - implementation of a fast multiplication algorithm
@@ -23,27 +20,28 @@ func getParts(x, n int) (int, int) {
 // Input: two n-digit positive integers x and y
 // Output: the product x * y
 // Assumptions: n is a power of 2
-func KaratsubaMult(x int, y int) (int, error) {
+func KaratsubaMult(x *big.Int, y *big.Int) (*big.Int, error) {
+	zero := big.NewInt(0)
 
-	if x == 0 || y == 0 {
-		return 0, nil
+	if x.Cmp(zero) == 0 || y.Cmp(zero) == 0 {
+		return zero, nil
 	}
 
-	xs := strconv.Itoa(x)
-	ys := strconv.Itoa(y)
+	xs := x.String()
+	ys := y.String()
+
 	// base case
 	if len(xs) == 1 || len(ys) == 1 {
-		return x * y, nil
+		result := new(big.Int).Mul(x, y)
+		return result, nil
 	} else {
-		// setup pivot points for number strings
-		// prepend zero if uneven number of digits
+		// check if we need to introduce padding
 		if len(xs)%2 != 0 {
 			xs = "0" + xs
 		}
 		if len(ys)%2 != 0 {
 			ys = "0" + ys
 		}
-
 		m := math.Max(float64(len(xs)), float64(len(ys)))
 		n := int(m / 1) // convert float to int
 		ndiv2 := n / 2
@@ -53,21 +51,27 @@ func KaratsubaMult(x int, y int) (int, error) {
 		c, d := getParts(y, ndiv2)
 		ac, err := KaratsubaMult(a, c)
 		if err != nil {
-			return 0, err
+			return zero, err
 		}
 		bd, err := KaratsubaMult(b, d)
 		if err != nil {
-			return 0, err
+			return zero, err
 		}
 		// compute p := a + b and q := c + d
-		p := a + b
-		q := c + d
+		p := new(big.Int).Add(a, b)
+		q := new(big.Int).Add(c, d)
 		pq, err := KaratsubaMult(p, q)
 		if err != nil {
-			return 0, err
+			return zero, err
 		}
-		adbc := pq - ac - bd
-		result := ac*int(math.Pow10(n)) + adbc*int(math.Pow10(ndiv2)) + bd
+		adbc := new(big.Int).Sub(pq, new(big.Int).Add(ac, bd))
+		powN := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(n)), nil)
+		powNdiv2 := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(ndiv2)), nil)
+
+		result := new(big.Int).Mul(ac, powN)
+		result.Add(result, new(big.Int).Mul(adbc, powNdiv2))
+		result.Add(result, bd)
+
 		return result, nil
 	}
 }
--- pkg/karatsuba_mult/karatsuba_mult_test.go
+++ pkg/karatsuba_mult/karatsuba_mult_test.go
@@ -1,27 +1,21 @@
 package karatsuba_mult_test
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/adammccartney/algorill/pkg/karatsuba_mult"
 )
 
 func TestKaratsubaMult(t *testing.T) {
-	var tests = []struct {
-		x, y, want int
-	}{
-		{134, 46, 6164},
-		{56, 12, 672},
-		{5678, 1234, 7006652},
-		{95678433, 10203033, 976210209287289},
-	}
+	x, _ := new(big.Int).SetString("1234567890123456789012345678901234567890123456789012345678901234", 10)
+	y, _ := new(big.Int).SetString("9876543210987654321098765432109876543210987654321098765432109876", 10)
+	want, _ := new(big.Int).SetString("12193263113702179522618503273386678859451150739156363359236761158176366412964425393011121414472580028964404791648155158039986984", 10)
 
-	for _, test := range tests {
-		got, err := karatsuba_mult.KaratsubaMult(test.x, test.y)
-		if err != nil {
-			t.Errorf("KaratsubaMult(%d, %d) error: %s", test.x, test.y, err)
-		} else if got != test.want {
-			t.Errorf("KaratsubaMult(%d, %d) = %d, want %d", test.x, test.y, got, test.want)
-		}
+	got, err := karatsuba_mult.KaratsubaMult(x, y)
+	if err != nil {
+		t.Errorf("KaratsubaMult(%s, %s) error: %s", x.String(), y.String(), err)
+	} else if got.Cmp(want) != 0 {
+		t.Errorf("KaratsubaMult(%s, %s) = %s, want %s", x.String(), y.String(), got.String(), want.String())
 	}
 }
