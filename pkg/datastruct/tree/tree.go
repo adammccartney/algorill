504544fddc38a1d39e6f4265dcd79fbd554c8031
Adam McCartney <adam@mur.at>
2023-11-06 08:15:04 +0100

    Add simple unit test for readProc
    Basically a place holder that will let us work out the rest of the
    functions. This won't get used to do any "real" work, just to figure out
    the mechanics of the call stack that needs to be implemented in order to
    read, construct and dump the tree.

--- pkg/datastruct/tree/tree.go
+++ pkg/datastruct/tree/tree.go
@@ -1,5 +1,11 @@
 package tree
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // Using a concrete type to sketch out a few general functions
 type Proc struct {
 	comm     string
@@ -30,4 +36,22 @@ func (list *Proc) addProc(comm string, pid int, uid int, ppid int) {
 func findProc() {}
 
 // Construct a tree by reading the proc pseudo-filesystem
-func readProc() {}
+// assumes that miniproc is a string containing records, separator=","
+func readProc(miniproc string) string {
+
+	var list *Proc
+	records := strings.Split(miniproc, ",")
+	for _, r := range records {
+		items := strings.Split(r, " ")
+		comm := items[0]
+		pid, _ := strconv.Atoi(items[1])
+		uid, _ := strconv.Atoi(items[2])
+		ppid, _ := strconv.Atoi(items[3])
+		list.addProc(comm, pid, uid, ppid)
+	}
+	return fmt.Sprint(list)
+}
+
+
+//		{name: "test construction logic for tree",
+//			args: args{"(systemd) 1 0 0, (ModemManager) 2 1 0, (abrd-dbug) 3 1 0"}},
---
+++ pkg/datastruct/tree/tree_test.go
@@ -0,0 +1,26 @@
+package tree
+
+import (
+	"testing"
+)
+
+func Test_readProc(t *testing.T) {
+	type args struct {
+		miniproc string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "test construction logic for tree",
+			args: args{"(systemd) 1 0 0, (ModemManager) 2 1 0, (abrd-dbug) 3 1 0"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := readProc(tt.args.miniproc); got != tt.want {
+				t.Errorf("readProc() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
